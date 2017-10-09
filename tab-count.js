9a28a9d90f4c915b959f7bcef62bbd783f9de123
Carl Johnson <me@carlmjohnson.net>
2017-10-09 18:51:26 -0400

    Tab count: More accurate stats, option to list tabs

--- tab-count.js
+++ tab-count.js
@@ -4,21 +4,35 @@ function run(argv) {
   let app = Application("Safari");
   app.includeStandardAdditions = true;
 
+  let active = 0;
   let total = 0;
   let urls = new Set();
+  let names = {};
 
   app.windows().forEach(w => {
     w.tabs().forEach(t => {
+      total++;
       let url = t.url();
-      if (url) total++;
-      urls.add(url);
+      if (url) {
+        active++;
+        urls.add(url);
+        names[url] = t.name();
+      }
     });
   });
 
   console.log("Safari Facts");
   console.log("------");
   console.log("Windows:", app.windows().length);
-  console.log("Tabs:", total);
+  console.log("Total tabs:", total);
+  console.log("Active tabs:", active);
   console.log("Distinct URLs:", urls.size);
+  console.log();
+  if (argv == "list") {
+    urls = Array.from(urls).sort();
+    urls.forEach((url, i) => {
+      console.log(`${i}. ${names[url]}
+    - ${url}`);
+    });
+  }
 }
-
